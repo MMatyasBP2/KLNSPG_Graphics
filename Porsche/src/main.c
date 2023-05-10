6d53be2fd27803219a32a48c6476fb7541e38b0b
MMatyasBP2 <matyika123456@gmail.com>
2023-05-10 18:49:57 +0200

    house moving to render_app added

--- Porsche/src/main.c
+++ Porsche/src/main.c
@@ -12,18 +12,6 @@ int main(int argc, char* argv[])
     init_app(&app, 1280, 720);
     while (app.is_running) {
         handle_app_events(&app);
-
-        if (app.house_state == HOUSE_MOVING_UP && app.scene.secondhousey < 100.0f)
-            app.scene.secondhousey += 0.1f;
-        else if (app.house_state == HOUSE_MOVING_UP)
-            app.house_state = HOUSE_STATIONARY;
-    
-
-        if (app.house_state == HOUSE_MOVING_DOWN && app.scene.secondhousey > -17.0f)
-            app.scene.secondhousey -= 0.1f;
-        else if (app.house_state == HOUSE_MOVING_DOWN)
-            app.house_state = HOUSE_STATIONARY;
-
         update_app(&app);
         render_app(&app);
     }
